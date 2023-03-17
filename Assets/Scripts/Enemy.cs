116a88ef056d7ffe4d1846fff63e5ee4e827a4e0
yan957 <yan957@gmail.com>
2023-03-18 00:25:09 +0300

    enemy movement added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,7 +6,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private float _hp = 20f;
+    private float _hp = 40f;
+    private float _speed = 20f;
+    private GameObject _target;
+
+    private void Awake()
+    {
+        _target = GameObject.FindGameObjectWithTag("BasePlace");
+    }
 
     private void TakeDamage(float damage)
     {
@@ -19,6 +26,12 @@ public class Enemy : MonoBehaviour
         {
             GlobalEventSystem.I.SendEvent(EventNames.Enemy.NonLethalDamaged, new GameEventArgs(null));
         }
+    }
+    
+    private void Update()
+    {
+        float step = _speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, _target.transform.position, step);
     }
 
     private void OnTriggerEnter2D(Collider2D col)
